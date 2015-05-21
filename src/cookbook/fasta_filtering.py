fe40af730d5f51a2fade757de0afbcfa837c297b
Eric Hervol <github@elkinsoft.com>
2015-05-21 10:52:49 -0700

    third pass fasta_filter

--- src/cookbook/fasta_filtering.py
+++ src/cookbook/fasta_filtering.py
@@ -4,22 +4,30 @@ from Bio import SeqIO
 
 from klab.process.lineage import create_taxonomy_data_structures, lineage_contains
 
-# 2015-05-15 ech - quick and dirty way to split out specific sub-trees from fasta files
-if __name__ == '__main__':
-    ncbi_dir = '../data'
-    node_dict, name_dict, merged_dict, deleted_list = create_taxonomy_data_structures(ncbi_dir)
 
+def filter_fasta_file(node_dict, input_file, output_file, filter_set):
     # for big files want to not store in memory but use sequence generators to deal with single records at a time
     matching_sequences = []
-    input_handle = open('/shared_projects/euk_fasta/18S.ref.fasta', 'rU')
+    input_handle = open(input_file, 'rU')
     for record in SeqIO.parse(input_handle, 'fasta'):
         tax_id = int(record.id.split('|')[0])
-        if lineage_contains(node_dict, tax_id, {2836, 2830}):
+        if lineage_contains(node_dict, tax_id, filter_set):
             matching_sequences.append(record)
     input_handle.close()
 
-    print "Found %i matching sequences" % len(matching_sequences)
+    print "Wrote %i matching sequences to %s" % (len(matching_sequences), output_file)
 
-    output_handle = open('/shared_projects/euk_fasta/output.fasta', 'w')
+    output_handle = open(output_file, 'w')
     SeqIO.write(matching_sequences, output_handle, "fasta")
     output_handle.close()
+
+
+# 2015-05-15 ech - quick and dirty way to split out specific sub-trees from fasta files
+if __name__ == '__main__':
+    ncbi_dir = '../data'
+    nodes, names, merged, deleted = create_taxonomy_data_structures(ncbi_dir)
+
+    filter_fasta_file(nodes, '/shared_projects/euk_fasta/18S.ref.fasta',
+                      '/shared_projects/euk_fasta/haptophyes.fasta', {2830})
+    filter_fasta_file(nodes, '/shared_projects/euk_fasta/18S.ref.fasta',
+                      '/shared_projects/euk_fasta/diatoms.fasta', {2836})

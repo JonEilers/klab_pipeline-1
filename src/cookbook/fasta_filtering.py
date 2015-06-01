ade538b0e4aa22848aecb8aeb303dc393319042a
Eric Hervol <github@elkinsoft.com>
2015-06-01 14:10:52 -0700

    fasta filter - tweak id and description again

--- src/cookbook/fasta_filtering.py
+++ src/cookbook/fasta_filtering.py
@@ -13,18 +13,16 @@ def filter_fasta_file(node_dict, name_dict, input_file, output_file, filter_set)
     for record in SeqIO.parse(input_handle, 'fasta'):
         tax_id = int(record.id.split('|')[0])
         if lineage_contains(node_dict, tax_id, filter_set):
-            # replace id and description for easier visualization
+            # move id to description for safe keeping
+            record.description = record.id
+            # replace id for better tree visualization
             record.id = str(tax_id) + '|' + string.replace(name_dict.get(tax_id), ' ', '_')
-            record.description = ''
             matching_sequences.append(record)
     input_handle.close()
-
-    print "Wrote %i matching sequences to %s" % (len(matching_sequences), output_file)
-
     output_handle = open(output_file, 'w')
     SeqIO.write(matching_sequences, output_handle, "fasta")
     output_handle.close()
-
+    print "Wrote %i matching sequences to %s" % (len(matching_sequences), output_file)
 
 # 2015-05-15 ech - quick and dirty way to split out specific sub-trees from fasta files
 if __name__ == '__main__':

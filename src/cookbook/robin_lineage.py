0d3f390508d41441e3bb918156981f50d6c6998e
Eric Hervol <github@elkinsoft.com>
2015-07-07 17:23:27 -0700

    one off for Robin

--- src/cookbook/robin_lineage.py
+++ src/cookbook/robin_lineage.py
@@ -12,7 +12,7 @@ from cookbook.mbari_lineage import create_mbari_lineage_files
 from klab.process.lineage import create_lineage
 
 
-def create_count_files(lineage, grouping, path):
+def _create_count_files(lineage, grouping, path):
     c = group_and_count(lineage, grouping)
     fuzzy_counts = c[c.placement_type == FUZZY]
     confident_counts = c[c.placement_type == CONFIDENT]
@@ -20,23 +20,23 @@ def create_count_files(lineage, grouping, path):
     write_df_to_file(confident_counts, path + 'count_confident.tsv')
 
 
-def create_mbari_files_for_robin(base):
+def _create_mbari_files_for_robin(base):
     l = create_mbari_lineage_files(base)
-    create_count_files(l, ['cluster', 'domain_name', 'size', 'location', 'placement_type'], base + 'domain_')
-    create_count_files(l, ['cluster', 'domain_name', 'division_name', 'size', 'location', 'placement_type'],
-                       base + 'division_')
-    create_count_files(l, ['cluster', 'domain_name', 'division_name', 'lowest_classification_name', 'size', 'location',
-                           'placement_type'], base + 'classification_')
+    _create_count_files(l, ['cluster', 'domain_name', 'size', 'location', 'placement_type'], base + 'domain_')
+    _create_count_files(l, ['cluster', 'domain_name', 'division_name', 'size', 'location', 'placement_type'],
+                        base + 'division_')
+    _create_count_files(l, ['cluster', 'domain_name', 'division_name', 'lowest_classification_name', 'size', 'location',
+                            'placement_type'], base + 'classification_')
 
 
-def create_placement_files_for_robin(ncbi_dir, input_dir, output_dir):
+def _create_lineage_and_count_files_for_robin(ncbi_dir, input_dir, output_dir):
     p = create_placements(dir=input_dir)
     l = create_lineage(ncbi_dir=ncbi_dir, placements=p,
                        out_file=os.path.join(output_dir, 'placements_with_lineage.tsv'))
-    create_count_files(l, ['domain_name', 'placement_type'], os.path.join(output_dir, 'domain_'))
-    create_count_files(l, ['domain_name', 'division_name', 'placement_type'], os.path.join(output_dir, 'division_'))
-    create_count_files(l, ['domain_name', 'division_name', 'lowest_classification_name', 'placement_type'],
-                       os.path.join(output_dir, 'classification_'))
+    _create_count_files(l, ['domain_name', 'placement_type'], os.path.join(output_dir, 'domain_'))
+    _create_count_files(l, ['domain_name', 'division_name', 'placement_type'], os.path.join(output_dir, 'division_'))
+    _create_count_files(l, ['domain_name', 'division_name', 'lowest_classification_name', 'placement_type'],
+                        os.path.join(output_dir, 'classification_'))
 
 
 if __name__ == '__main__':
@@ -47,4 +47,4 @@ if __name__ == '__main__':
     input_dir = sys.argv[2]
     output_dir = sys.argv[3]
 
-    create_placement_files_for_robin(ncbi_dir, input_dir, output_dir)
+    _create_lineage_and_count_files_for_robin(ncbi_dir, input_dir, output_dir)

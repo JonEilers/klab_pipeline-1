bed826f9152ef1bb7f6ffd4033643131fb986573
Ryan <mclaughlinr2@gmail.com>
2016-03-02 18:10:47 -0800

    added countbot, re-added placement type to annotations, changed cluster to gene, added demo data

--- src/klab/process/countBot.revenge.py
+++ src/klab/process/countBot.revenge.py
@@ -1,20 +1,20 @@
-# Scavenged code from Eric's mbari_lineage.py
+#!/usr/bin/env python
+# Built by Eric Hervol, modified by Ryan McLaughlin
 import sys
-sys.path.append('/home/ryan/Dropbox/Ryan_Grad/collabs/KLab/klab_pipeline/src/klab/process')
-from derived_info import group_and_count, FUZZY, CONFIDENT
-from file_manager import create_placements, write_df_to_file
-from lineage import create_lineage
+from klab.process.derived_info import group_and_count, FUZZY, CONFIDENT
+from klab.process.file_manager import create_placements, write_df_to_file
+from klab.process.lineage import create_lineage
 from pandas import DataFrame
 
 
-def create_lineage_files(base):
-    jplace_dir = base
-    lineage_file = base + '_placements_with_lineage.tsv'
+def create_lineage_files(jpath):
+    jplace_dir = jpath
+    lineage_file = jpath + '_placements_with_lineage.tsv'
 
     p = create_placements(dir=jplace_dir)
-    l = create_lineage(ncbi_dir='/home/ryan/Dropbox/Ryan_Grad/collabs/KLab/klab_pipeline/src/data', placements=p)
+    l = create_lineage(placements=p)
     write_df_to_file(l, lineage_file)
-    return l
+    return l, lineage_file
 
 
 def create_and_write_count_files(lineage, grouping, path):
@@ -25,18 +25,17 @@ def create_and_write_count_files(lineage, grouping, path):
     write_df_to_file(confident_counts, path + 'count_confident.tsv')
 
 
-def do_the_do(base):
-    l = create_lineage_files(base)
-    l = DataFrame.from_csv('/media/ryan/PI/WDFW_data/recirc/place_files_placements_with_lineage.tsv', sep='\t')
-    create_and_write_count_files(l, ['cluster', 'domain_name', 'placement_type'], base + '_domain_')
-    create_and_write_count_files(l, ['cluster', 'domain_name', 'division_name', 'placement_type'],
-                                 base + '_division_')
-    create_and_write_count_files(l, ['cluster', 'domain_name', 'division_name', 'class_name',
-                                     'placement_type'], base + '_class_')
-    create_and_write_count_files(l, ['cluster', 'domain_name', 'division_name', 'class_name','lowest_classification_name',
-                                     'placement_type'], base + '_lowest_classification_')
+def do_the_do(jpath):
+    lin, lineage_file = create_lineage_files(jpath)
+    create_and_write_count_files(lin, ['gene', 'domain_name', 'placement_type'], jpath + '_domain_')
+    create_and_write_count_files(lin, ['gene', 'domain_name', 'division_name', 'placement_type'],
+                                 jpath + '_division_')
+    create_and_write_count_files(lin, ['gene', 'domain_name', 'division_name', 'class_name',
+                                     'placement_type'], jpath + '_class_')
+    create_and_write_count_files(lin, ['gene', 'domain_name', 'division_name', 'class_name','lowest_classification_name',
+                                     'placement_type'], jpath + '_lowest_classification_')
 
 
-# rjm [ech] 2015-03-19 - take the repetition out of rebuilding the ETSP data
 if __name__ == '__main__':
-	do_the_do('/media/ryan/PI/WDFW_data/recirc/place_files')
+	jpath = '/home/ryan/Dropbox/PycharmProjects/klab_pipeline/src/demo/jplace'
+	do_the_do(jpath)

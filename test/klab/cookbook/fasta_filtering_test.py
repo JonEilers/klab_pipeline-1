4c3afd220635417ac57ac683af605932e9ac5cca
peenjo <peenjo@elkinsoft.com>
2016-11-17 16:13:20 -0800

    changed unit tests to pytest from unittest

--- test/klab/cookbook/fasta_filtering_test.py
+++ test/klab/cookbook/fasta_filtering_test.py
@@ -1,25 +1,23 @@
-import unittest
+import pytest
 
 from cookbook.fasta_filtering import _lineage_contains, _turn_ncbi_name_into_file_name, _split_fasta_file
 
 
-class TestFastaFiltering(unittest.TestCase):
-    def test_lineage_contains(self):
-        self.assertFalse(_lineage_contains([1, 131567, 2, 18, 19], {5}))
-        self.assertTrue(_lineage_contains([1, 131567, 2, 18, 19], {2}))
-        self.assertTrue(_lineage_contains([1, 131567, 2, 18, 19], {2, 5}))
-        self.assertTrue(_lineage_contains([1, 131567, 2, 6, 7, 9], {2, 5}))
+def test_lineage_contains():
+    assert not _lineage_contains([1, 131567, 2, 18, 19], {5})
+    assert _lineage_contains([1, 131567, 2, 18, 19], {2})
+    assert _lineage_contains([1, 131567, 2, 18, 19], {2, 5})
+    assert _lineage_contains([1, 131567, 2, 6, 7, 9], {2, 5})
 
-    def test_turn_ncbi_name_into_file_name(self):
-        self.assertEqual('this_is_a-test_so_there', _turn_ncbi_name_into_file_name('This is/a-Test.so&There'))
-        self.assertEqual('bacillus_sp_a21_2015', _turn_ncbi_name_into_file_name('Bacillus sp. A21(2015)'))
-        self.assertEqual('influenza_a_virus_a_swine_south_dakota_a01481942_2014_h1n1',
-                         _turn_ncbi_name_into_file_name(
-                             'Influenza A virus (A/swine/South Dakota/A01481942/2014(H1N1))'))
 
-    def test_split_fasta_file(self):
-        try:
-            _split_fasta_file('no_file', None, None, None)
-            self.fail('should have thrown ValueError')
-        except ValueError as ve:
-            self.assertEqual('no_file is not a file.', ve.message)
+def test_turn_ncbi_name_into_file_name():
+    assert 'this_is_a-test_so_there' == _turn_ncbi_name_into_file_name('This is/a-Test.so&There')
+    assert 'bacillus_sp_a21_2015' == _turn_ncbi_name_into_file_name('Bacillus sp. A21(2015)')
+    assert 'influenza_a_virus_a_swine_south_dakota_a01481942_2014_h1n1' == \
+           _turn_ncbi_name_into_file_name('Influenza A virus (A/swine/South Dakota/A01481942/2014(H1N1))')
+
+
+def test_split_fasta_file():
+    with pytest.raises(ValueError) as ve:
+        _split_fasta_file('no_file', None, None, None)
+    assert 'no_file is not a file.' in str(ve.value)

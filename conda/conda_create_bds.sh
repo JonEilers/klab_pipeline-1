8516597ed0147a60880b4d6be5506cffa9eeddab
peenjo <peenjo@elkinsoft.com>
2016-11-28 22:39:53 -0800

    sudden package reference issue with anaconda and python2.7

--- conda/conda_create_bds.sh
+++ conda/conda_create_bds.sh
@@ -3,10 +3,11 @@
 # This creates a conda environment for biological data science (bds). Note that it can create python 2.7x or
 # python 3.5x environments. The default is python 2.7x
 
-conda create -y -n bds anaconda biopython scikit-bio seaborn python=2
+conda create -y -n bds biopython scikit-bio seaborn python
 source activate bds
 
 # TODO ech 2016-11-22 - write switch code for p3.5
+#conda create -y -n bds3 anaconda biopython scikit-bio seaborn python=3
 
 pip install simplejson jupyter seqmagick pytest coverage mock
 
